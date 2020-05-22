66fbf99ee1f3e0d67dc4d0470a376a48b05b6d00
reggae93 <35476328+reggae93@users.noreply.github.com>
2020-05-21 23:48:41 -0300

    Add files via upload

---
+++ suma.py
@@ -0,0 +1,10 @@
+print("Ingreso De dos Numeros y su Suma")
+n1=int(input("Ingrese el Primer Numero: "))
+n2=int(input("Ingrese el Segundo Numero: "))
+
+suma = n1+n2;
+
+print("La suma es : " + str(suma))
+
+print("Felicidades Sumo los dos numeros correctamente :D")
+
